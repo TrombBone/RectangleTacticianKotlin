764f012f8fff16088beba79ff39635c30be2cb6c
kruch <kruchinin-kostya@mail.ru>
2021-05-31 19:41:37 +0300

    Initial commit. Stub for tests

---
+++ app/src/androidTest/java/com/example/rectangletacticiankotlin/MyAppDataStub.kt
@@ -0,0 +1,9 @@
+package com.example.rectangletacticiankotlin
+
+class MyAppDataStub(
+    playerCount: Int = PLAYER_COUNT_DEFAULT,
+    fieldWidth: Int = FIELD_WIDTH_DEFAULT,
+    fieldHeight: Int = FIELD_HEIGHT_DEFAULT
+) : MyAppData(playerCount, fieldWidth, fieldHeight) {
+
+}
